f1ea4cb4fa5b130b6dc94ceee1ff1deada486232
kmzkmzh <167289873+kmzkmzh@users.noreply.github.com>
2024-06-05 15:53:04 +0900

    Add files via upload

---
+++ script.js
@@ -0,0 +1,12 @@
+// JavaScript
+// console.log('Hello world!');
+
+$(function () {
+    $('.bxslider').bxSlider({
+        auto: true,
+        autoControls: true,
+        stopAutoOnClick: true,
+        pager: true,
+        slideWidth: 1200
+    });
+});
